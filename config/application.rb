aa5b9b6e2ab96707156936a28980957e0dbf7599
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2017-04-09 00:40:27 -0300

    Add racks core configuration for login

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,13 @@ module GuimifiuBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Flash
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, except: [:new, :index, :edit] do
+      resources :users, except: [:new, :index, :edit, :show] do
       end
       post 'users/exists'   => 'users#provider_exists'
       post 'users/sign_in'  => 'users#sign_in'
