66f7c9147777880bcc9a0b6aad396af718eb89b0
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2017-05-10 23:12:14 -0300

    Adds codeclimate badge and fixes some code issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include ActionController::RequestForgeryProtection
-  
+
   protect_from_forgery with: :null_session
   before_action :authenticate
 
   def authenticate
-    authenticate_or_request_with_http_token do |token, options|
+    authenticate_or_request_with_http_token do |token, _options|
       ApiUser.find_by(auth_token: token)
     end
   end
