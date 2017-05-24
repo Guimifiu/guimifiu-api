7a50b3d480f4b8251144e2cbda11596e5059cc29
Marco Silva <marcowilliamdf@gmail.com>
2017-05-24 15:53:38 -0300

    Add db documentation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405132726) do
+ActiveRecord::Schema.define(version: 20170524184057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20170405132726) do
     t.string   "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "gas_stations", force: :cascade do |t|
+    t.string   "latitude"
+    t.string   "longitude"
+    t.integer  "google_maps_id"
+    t.string   "vicinity"
+    t.string   "name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
