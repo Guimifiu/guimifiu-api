684089fd0cc8834b59890601529b1953f9f54e18
Marco Silva <marcowilliamdf@gmail.com>
2017-11-07 17:12:18 -0200

    Change admin password

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,8 +4,8 @@ config.authorize_with do
     authenticate_or_request_with_http_basic('Login required') do |username, password|
     #   username == Rails.application.secrets.user &&
     #   password == Rails.application.secrets.password
-      username == "admin" &&
-      password == "12345678"
+      username == ENV["ADMIN_LOGIN"] &&
+      password == ENV["ADMIN_PASSWORD"]
     end
   end
 
